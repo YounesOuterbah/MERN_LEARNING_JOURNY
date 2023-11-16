2c99624a76c1b2d3af1437b3face0e67287285ff
Younes Outerbah <younesouterbah1@gmail.com>
2023-11-16 16:16:00 +0100

    editing here and there

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const express = require('express')
+const { log } = require("console");
+const express = require("express");
 
-const app = express()
+const app = express();
+log(express);
 
-app.listen(3000,() => {console.log('am listening at port 3000 ')})
+app.listen(3000, () => {
+  console.log("am listening at port 3000 ");
+});
 /// you can use nodemon to dont restart the server each time you made changes

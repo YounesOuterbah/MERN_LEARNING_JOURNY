89e852784ac90e5ae1d783ea7c26d93b5698cf65
Younes Outerbah <younesouterbah1@gmail.com>
2023-11-18 20:51:54 +0100

    adding users via server

--- app.js
+++ app.js
@@ -2,8 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json());
+
+const users = [];
+
+app.get("/", (req, res) => {
+  res.send("Welcome Home");
+});
+
+app.get("/users", (req, res) => {
+  if (users.length == 0) {
+    res.status(404).send("no users found");
+  }
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  users.push(req.body);
+  res.status(201).send("Created!");
 });
 
 app.listen("5000", () => {

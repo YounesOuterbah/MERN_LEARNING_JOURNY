978029c5fd7be52ce4e77047547f961bfdfed580
Younes Outerbah <younesouterbah1@gmail.com>
2023-11-18 18:32:45 +0100

    added git igonre file

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-const { log } = require("console");
-const express = require("express");
-
-const app = express();
-log(express);
-
-app.listen(3000, () => {
-  console.log("am listening at port 3000 ");
-});
+console.log("hola bola");
 /// you can use nodemon to dont restart the server each time you made changes

23015005958a72cd296c0e4bdffb94dbcaf8a582
Younes Outerbah <younesouterbah1@gmail.com>
2023-11-26 08:14:51 +0100

    added express async handler

--- routes/authors.js
+++ routes/authors.js
@@ -1,29 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const { Author } = require("../models/Authors");
+const asyncHandler = require("express-async-handler");
 
-router.get("/", async (req, res) => {
-  try {
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
     const authorList = await Author.find();
     res.send(authorList);
-  } catch (error) {
-    console.log(error);
-    res.status(500);
-  }
-});
+  })
+);
 
-router.get("/:id", async (req, res) => {
-  try {
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
     const author = await Author.findById(req.params.id);
     author ? res.status(200).json(author) : res.status(404).json({ message: "Author Not Found" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Something Went Wrong" });
-  }
-});
+  })
+);
 
-router.post("/", async (req, res) => {
-  try {
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
     const author = new Author({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
@@ -34,14 +32,12 @@ router.post("/", async (req, res) => {
     const result = await author.save();
 
     res.status(200).json(result);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Something Went Wrong" });
-  }
-});
+  })
+);
 
-router.put("/:id", async (req, res) => {
-  try {
+router.put(
+  "/:id",
+  asyncHandler(async (req, res) => {
     const author = await Author.findByIdAndUpdate(
       req.params.id,
       {
@@ -56,19 +52,18 @@ router.put("/:id", async (req, res) => {
     );
 
     res.status(200).json(author);
-  } catch (error) {
-    console.log(error);
-  }
-});
+  })
+);
 
-router.delete("/:id", async (req, res) => {
-  try {
+router.delete(
+  "/:id",
+  asyncHandler(async (req, res) => {
     const author = await Author.findById(req.params.id);
     author
       ? (await Author.findByIdAndDelete(req.params.id),
         res.status(200).json({ message: "author has been deleted" }))
       : res.status(404).json({ message: "author not found" });
-  } catch (error) {}
-});
+  })
+);
 
 module.exports = router;
